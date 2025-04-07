ff662b14b56f482be8fadebb44da60f643aa7863
clim-lumiq <clim@lumiq.com>
2025-04-07 15:17:02 +0800

    add output for tenant_id in azuread_client_config

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ data "azuread_client_config" "current" {}
 output "name" {
   value = data.azuread_client_config.current.client_id
 }
+
+output "tenant" {
+  value = data.azuread_client_config.current.tenant_id
+}
