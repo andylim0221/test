ba31971e85f1d04eeb4ce25b12051e8cae0ed4c3
clim-lumiq <clim@lumiq.com>
2025-04-22 15:33:00 +0800

    testing

--- main.tf
+++ main.tf
@@ -1,13 +1,51 @@
-data "azuread_client_config" "current" {}
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+provider "azuread" {
+}
+
+data "azuread_application_published_app_ids" "well_known" {}
 
 output "name" {
-  value = data.azuread_client_config.current.client_id
+  value = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+
 }
 
-output "tenant" {
-  value = data.azuread_client_config.current.tenant_id
+resource "azuread_service_principal" "msgraph" {
+  application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+  use_existing   = true
 }
 
-output "object" {
-  value = data.azuread_client_config.current.object_id
+resource "azuread_application" "example" {
+  display_name = "thisisatesting"
+
+  required_resource_access {
+    resource_app_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+
+    resource_access {
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["Application.ReadWrite.OwnedBy"]
+      type = "Scope"
+    }
+
+    resource_access {
+      id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids["User.Read.All"]
+      type = "Scope"
+    }
+  }
+}
+
+resource "azuread_service_principal" "example" {
+  application_id = azuread_application.example.application_id
+}
+
+resource "azuread_service_principal_delegated_permission_grant" "example" {
+  service_principal_object_id          = azuread_service_principal.example.object_id
+  resource_service_principal_object_id = azuread_service_principal.msgraph.object_id
+  claim_values                         = ["Application.ReadWrite.OwnedBy", "User.Read.All"]
 }
